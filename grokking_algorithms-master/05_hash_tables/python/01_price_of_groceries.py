dd6497bf7cb6019daa82a0bf79f5ce5e5d8fd85e
Andy.Ren <apple@pc-67-122.customer.ask4.lan>
2019-11-07 01:32:52 +0000

    hash table

--- grokking_algorithms-master/05_hash_tables/python/01_price_of_groceries.py
+++ grokking_algorithms-master/05_hash_tables/python/01_price_of_groceries.py
@@ -4,4 +4,10 @@ book["apple"] = 0.67
 # milk costs $1.49
 book["milk"] = 1.49
 book["avocado"] = 1.49
-print book
+print (book)
+
+
+
+
+
+
--- grokking_algorithms-master/05_hash_tables/python/02_check_voter.py
+++ grokking_algorithms-master/05_hash_tables/python/02_check_voter.py
@@ -1,11 +1,45 @@
 voted = {}
 def check_voter(name):
   if voted.get(name):
-    print "kick them out!"
+    print ('kick them out!')
   else:
     voted[name] = True
-    print "let them vote!"
+    print ('let them vote!')
 
 check_voter("tom")
 check_voter("mike")
 check_voter("mike")
+
+
+food={'banana':1.2,'tomato':2,'price':'fair'}
+food['where']='market'
+print(food.get('banana'))
+for i in food:
+    print(food[i])
+
+def vote_check(name,dic):
+    if name in dic:
+        print('go away')
+    else:
+        dic[name]='1'
+        print('let him vote')
+vote_check('banana',food)
+vote_check('watermelon',food)
+vote_check('watermelon',food)
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily['child1'])
+print(myfamily['child1']['name'])
