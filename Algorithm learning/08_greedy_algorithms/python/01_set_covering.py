750e171bf9224a046019ca72967f00cd72a96b62
S.Ren <55898103+andy01248@users.noreply.github.com>
2019-11-26 02:07:24 +0000

    greedy

--- Algorithm learning/08_greedy_algorithms/python/01_set_covering.py
+++ Algorithm learning/08_greedy_algorithms/python/01_set_covering.py
@@ -22,4 +22,42 @@ while states_needed:
   states_needed -= states_covered
   final_stations.add(best_station)
 
-print final_stations
+print (final_stations)
+
+
+print(set([1,1,6,3,2,2,2,3,3,4,3,4,4,5,3,3]))
+print(set([6,6,4,4,2,2,3,3,1,1,2]))
+print(set([1,2,3,4]-))
+print(set(['a','a','b','b','c','c','d','d','f','f','e','e']))
+
+
+
+
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+chosen_station=set()
+
+while states_needed:
+  best_station=None 
+  best_covered=set()
+  for current_station in stations.keys():
+    current_covered = stations[current_station] & states_needed
+    if len(current_covered) > len(best_covered):
+      best_station=current_station
+      best_covered=current_covered
+  chosen_station.add(best_station)
+  states_needed -= best_covered #this is the set difference -> delete the similar item between two set from set1 
+  
+print(chosen_station)
+
+a=[1,2,3]
+b=[2,3,4]
+print(a+b)
+diff(a,b)
